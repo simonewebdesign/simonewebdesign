528955cf5e5f6fbcf0c84b768ddef399cb7ccd60
Simone Vittori <svitto@svit.to>
2020-07-04 18:28:16 +0100

    Add schema

---
+++ scripts/schema.sql
@@ -0,0 +1,17 @@
+-- This is the schema of simo's MariaDB in Heroku
+
+CREATE TABLE `users` (
+  `email` varchar(320) CHARACTER SET utf8 NOT NULL,
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+
+CREATE TABLE `requests` (
+    id INT(7) NOT NULL AUTO_INCREMENT,
+    `path` varchar(2000) NOT NULL,
+    `status_code` char(3) NOT NULL,
+    -- MariaDB automatically assigns the following properties to the column:
+    -- DEFAULT CURRENT_TIMESTAMP
+    -- ON UPDATE CURRENT_TIMESTAMP
+    `timestamp` timestamp NOT NULL,
+    PRIMARY KEY (id)
+)
