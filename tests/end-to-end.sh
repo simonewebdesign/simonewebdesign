395e6b2cdda00ce32f98fca8591f6591e314dfb1
Simone <hello@simonewebdesign.it>
2020-11-21 21:10:30 +0000

    Update end-to-end tests

--- tests/end-to-end.sh
+++ tests/end-to-end.sh
@@ -26,7 +26,7 @@ function test {
 
 # Home page and core assets
 test "" "Simone Web Design"
-test stylesheets/style.css "html\{-moz-osx-font-smoothing:grayscale;-webkit-font-smoothing:antialiased;background:#f6f6f6 url\("
+test stylesheets/style.css "html\{-moz-osx-font-smoothing:grayscale;-webkit-font-smoothing:antialiased;background:#f6f6f6"
 test sw.js 'self.addEventListener\("install"'
 test stylesheets/about.css ".about-intro picture{float:left;"
 test stylesheets/projects.css ".projects section\+section{margin-top:"
