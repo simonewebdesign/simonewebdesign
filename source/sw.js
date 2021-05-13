625d0ff2287f7b8a3238e6863a86ea612adbc391
Simone <hello@simonewebdesign.it>
2021-05-13 22:58:53 +0100

    Restore sw.js and offline page

--- source/sw.js
+++ source/sw.js
@@ -1,57 +1,31 @@
-self.addEventListener("install", function(event) {
-  event.waitUntil(preLoad());
-});
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('v1').then(cache =>
+      cache.addAll([
+        '/',
+        '/archives/',
+        '/projects/',
+        '/about/',
+        '/images/simonewebdesign.webp',
+        '/offline/'
+      ])
+    )
+  )
+})
 
-var preLoad = function(){
-  return caches.open("offline").then(function(cache) {
-    return cache.addAll([
-      "/",
-      "/archives/",
-      "/projects/",
-      "/about/",
-      "/404/",
-    ]);
-  });
-};
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => {
+      return resp || fetch(e.request).then(response => {
+        let responseClone = response.clone()
+        caches.open('v1').then((cache) => {
+          cache.put(e.request, responseClone)
+        })
 
-self.addEventListener("fetch", function(event) {
-  if (event.request.method != 'GET') return;
-  event.respondWith(checkResponse(event.request).catch(function() {
-    return returnFromCache(event.request);
-  }));
-  event.waitUntil(addToCache(event.request));
-});
-
-var checkResponse = function(request){
-  return new Promise(function(fulfill, reject) {
-    fetch(request).then(function(response){
-      if(response.status !== 404) {
-        fulfill(response);
-      } else {
-        reject();
-      }
-    }, reject);
-  });
-};
-
-var addToCache = function(request){
-  return caches.open("offline").then(function (cache) {
-    return fetch(request).then(function (response) {
-      return cache.put(request, response);
-    });
-  });
-};
-
-var returnFromCache = function(request){
-  return caches.open("offline").then(function (cache) {
-    return cache.match(request).then(function (matching) {
-      if(!matching || matching.status == 404) {
-        if (navigator.onLine) {
-          return cache.match("404/");
-        }
-      } else {
-        return matching;
-      }
-    });
-  });
-};
+        return response
+      }).catch(() => {
+        return caches.match('/offline/')
+      })
+    })
+  )
+})
