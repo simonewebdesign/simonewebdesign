c20d3880f3ce5725e2f8c94e00313a517e6f1441
Simone <hello@simonewebdesign.it>
2021-05-13 22:43:34 +0100

    Revert "Replace ServiceWorker caching with a friendly console message"
    This reverts commit 67095d63bf7bb26319790c06c853ec12810f0aa7.

--- source/sw.js
+++ source/sw.js
@@ -1,1 +1,57 @@
-self.addEventListener('install',function(){console.log('Hi, fellow developer! Feel free to look around ^_^')})
+self.addEventListener("install", function(event) {
+  event.waitUntil(preLoad());
+});
+
+var preLoad = function(){
+  return caches.open("offline").then(function(cache) {
+    return cache.addAll([
+      "/",
+      "/archives/",
+      "/projects/",
+      "/about/",
+      "/404/",
+    ]);
+  });
+};
+
+self.addEventListener("fetch", function(event) {
+  if (event.request.method != 'GET') return;
+  event.respondWith(checkResponse(event.request).catch(function() {
+    return returnFromCache(event.request);
+  }));
+  event.waitUntil(addToCache(event.request));
+});
+
+var checkResponse = function(request){
+  return new Promise(function(fulfill, reject) {
+    fetch(request).then(function(response){
+      if(response.status !== 404) {
+        fulfill(response);
+      } else {
+        reject();
+      }
+    }, reject);
+  });
+};
+
+var addToCache = function(request){
+  return caches.open("offline").then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response);
+    });
+  });
+};
+
+var returnFromCache = function(request){
+  return caches.open("offline").then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      if(!matching || matching.status == 404) {
+        if (navigator.onLine) {
+          return cache.match("404/");
+        }
+      } else {
+        return matching;
+      }
+    });
+  });
+};
