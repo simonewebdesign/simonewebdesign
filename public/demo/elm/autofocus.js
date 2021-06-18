033cc237638adfd7bb1ca3ae43b5e5a234ec2ca3
Simone <hello@simonewebdesign.it>
2021-06-18 22:19:57 +0100

    rm -r public/ --cached

--- public/demo/elm/autofocus.js
+++
@@ -1,8 +0,0 @@
-var observer = new MutationObserver(function(mutations) {
-  mutations.forEach(function(mutation) {
-    mutation.target.focus();
-  });
-});
-var target = document.querySelector('.card-number-inputs');
-var config = { attributes: true, subtree: true };
-observer.observe(target, config);
