e90aae87176893ca3304aa6950b760782618074d
Simone <hello@simonewebdesign.it>
2023-03-23 17:59:33 +0000

    Bump service worker cache

--- source/sw.js
+++ source/sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('v7').then(cache =>
+    caches.open('v8').then(cache =>
       cache.addAll([
         '/archives/',
         '/projects/',
@@ -12,18 +12,12 @@ self.addEventListener('install', e => {
   )
 })
 
-self.addEventListener('activate', e => {
-  ['v1','v2','v3','v4','v5','v6'].forEach(c => {
-    caches.delete(c)
-  })
-})
-
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(resp => {
       return resp || fetch(e.request).then(response => {
         let responseClone = response.clone()
-        caches.open('v7').then((cache) => {
+        caches.open('v8').then((cache) => {
           cache.put(e.request, responseClone)
         })
 
