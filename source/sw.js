90ab2c3def47dc841a1c65a09f4f7b7b35c4bc45
Simone <hello@simonewebdesign.it>
2023-04-01 18:46:45 +0100

    Cache all pages in ServiceWorker

--- source/sw.js
+++ source/sw.js
@@ -1,12 +1,27 @@
+---
+---
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open('v8').then(cache =>
       cache.addAll([
-        '/archives/',
-        '/projects/',
-        '/about/',
-        '/images/simonewebdesign.webp',
-        '/offline/'
+'/',
+'/archives/',
+'/projects/',
+'/about/',
+'/images/simonewebdesign.webp',
+'/categories/javascript/',
+'/categories/ruby/',
+'/categories/bash/',
+'/categories/git/',
+'/categories/css/',
+'/categories/elixir/',
+'/categories/swift/',
+'/categories/php/',
+'/categories/elm/',
+'/categories/clojure/',
+'/categories/rust/',
+{% for post in site.posts %}'{{ post.url }}'{% if forloop.last %}{% else %},
+{% endif %}{% endfor %}
       ])
     )
   )
@@ -15,8 +30,6 @@ self.addEventListener('install', e => {
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-    .then(res =>
-      res || fetch(event.request)
-    )
+    .then(res => res || fetch(event.request))
   )
 })
