e13c929180f4dd1e028a730466533eec507209be
Simone <hello@simonewebdesign.it>
2023-11-10 13:50:33 +0000

    Bump sw cache

--- source/sw.js
+++ source/sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('20231008').then(cache =>
+    caches.open('20231110').then(cache =>
       cache.addAll(['/archives/', '/projects/', '/about/', '/images/simonewebdesign.webp', '/offline/'])
     )
   )
